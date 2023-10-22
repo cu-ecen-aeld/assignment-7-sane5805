cd6902fab614a5831235a83da32683a406dd3b25
Saurav Negi <sane5805@colorado.edu>
2023-10-22 01:59:28 -0600

    scull & module load changes and hello.c changes

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,13 +7,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, world I am sane5805\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, cruel world I am sane5805\n");
 }
 
 module_init(hello_init);
